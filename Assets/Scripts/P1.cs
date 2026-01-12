4d0a4c36cd541271946491aa20f734fc417350d5
Ethan-des <ethanburnesMKDM@gmail.com>
2026-01-11 17:46:41 -0800

    Ultimate Handball
    This is what I have so far of my Unity side project Ultimate Handball. This a modified version of pong where it is more about how many times you can bounce the ball on your paddle until you miss. All that is missing is a score system, a high score system, lives, and win and lose conditions.

---
+++ Assets/Scripts/P1.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1 : MonoBehaviour
+{
+    public float boundY = 4.75f;//Determines where the end of the screen is
+    public float moveSpeed = 5f;//Determines the speed of the paddle
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isPressingUp = Input.GetKey(KeyCode.UpArrow);
+        bool isPressingDown = Input.GetKey(KeyCode.DownArrow);
+
+        if(isPressingUp)
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
+        }
+
+        if (isPressingDown)
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * moveSpeed);
+        }
+
+        //to keep inside the game screen
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
